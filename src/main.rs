f2111694f7ad8d6b6777182af1e9ef9311a0af48
Casimir Rönnlöf <casimirr04@gmail.com>
2026-04-21 18:46:06 +0300

    more stuff

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 fn main() {
-    //println!("Hello, world!");
-    let inputs: [f32; 3] = [1.0,2.0,3.0];
-    let weights: [f32; 3] = [0.2, 0.8, -0.5];
-    let bias: f32 = 2.0;
+    multiple_neurons();
 }
 
 fn single_neuron() {
@@ -15,4 +12,26 @@ fn single_neuron() {
     println!("{result}");
 }
 
-fn multiple_neurons()
+fn multiple_neurons() {
+    let inputs: [f32; 4] = [1.0,2.0,3.0,2.5];
+    let weights: [[f32; 4]; 3] = [[0.2, 0.8, -0.5, 1.0],[0.5, -0.91, 0.26, -0.5],[-0.26, -0.27, 0.17, 0.87]];
+    let biases: [f32; 3] = [2.0,3.0,0.5];
+
+    let mut layer_outputs = vec![];
+    for i in 0..weights.len(){
+        //println!("{}", layer_outputs[i]);
+
+        let mut neuron_output: f32 = 0.0;
+
+        for j in 0..weights[i].len(){
+            neuron_output += inputs[j]*weights[i][j];
+        }
+
+        neuron_output += biases[i];
+        layer_outputs.push(neuron_output);
+    }
+
+    for output in layer_outputs {
+        println!("{}", output)
+    }
+}
